3e0ee32458dbf579247173d3da89a192d2715280
bulbflux <bulbflux@gmail.com>
2025-11-30 19:44:48 +0000

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/site/',   // 👈 OBRIGATÓRIO para GitHub Pages no repo "site"
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
